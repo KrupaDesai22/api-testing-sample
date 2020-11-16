2d4555be9a4bf46f5b53c6287d0ad31ed6d59b5d
shridhar kalagi <shridharkalagi@gmail.com>
2020-11-16 23:02:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+`yarn && yarn test` 
+
+
+Fake server - `https://my-json-server.typicode.com/`
---
+++ sample.test.js
@@ -0,0 +1,52 @@
+import apiHelper from "./apiHelper";
+import chai from "chai";
+const expectChai = chai.expect;
+const assertChai = chai.assert;
+import schema_getAllPosts from "./schemas/getAllPosts.json";
+import schema_getSinglePost from "./schemas/getSinglePost.json";
+
+import Ajv from "ajv";
+
+const url = "https://jsonplaceholder.typicode.com";
+let ajv = new Ajv({ allErrors: true });
+
+test("Validate the posts", async () => {
+  const uri = "/posts";
+  let responseBody;
+  await apiHelper
+    .get(url, uri, {}, {})
+    .expect(200)
+    .expect((response) => {
+      responseBody = response.body;
+      expectChai(response.body.length).to.be.at.least(100);
+    });
+
+  let validate = ajv.compile(schema_getAllPosts);
+  let valid = validate(responseBody);
+
+  if (valid) console.log("Schema is valid!");
+  else console.log("Invalid: " + ajv.errorsText(validate.errors));
+  expectChai(valid).to.be.true; //Fail the test if schema validation fails
+});
+
+test("Validate a single post", async () => {
+  const id = "1"
+  const uri = "/posts/"+id;
+  let responseBody;
+  await apiHelper
+    .get(url, uri, {}, {})
+    .expect(200)
+    .expect((response) => {
+      responseBody = response.body;
+      assertChai.isNotArray(response.body);
+      //   assertChai(response.body.id).to.equal(1);
+      expectChai(response.body.id+"").to.equal(id);
+    });
+
+  let validate = ajv.compile(schema_getSinglePost);
+  let valid = validate(responseBody);
+
+  if (valid) console.log("Schema is valid!");
+  else console.log("Invalid: " + ajv.errorsText(validate.errors));
+  expectChai(valid).to.be.true; //Fail the test if schema validation fails
+});
